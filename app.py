db31834eff8601d9b0b2b965e015af86ff20f496
tokoandi <yellowtiger417@gmail.com>
2023-08-08 14:08:52 +0700

    .

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Markup
 
 app = Flask(__name__)
+
 
 # Pindahkan kode pembacaan "kbbi.txt" ke sini
 with open("kbbi.txt") as f:
@@ -19,16 +20,22 @@ def find_matches(s):
 @app.route("/", methods=["GET", "POST"])
 def index():
     result = ""
+    error_message = ""
 
     if request.method == "POST":
         input_text = request.form["input_text"]
-        matches = find_matches(input_text)
-        if matches:
-            result = "\n".join(matches)
-        else:
-            result = "Tidak ada hasil ditemukan"
 
-    return render_template("index.html", result=result)
+        # Validasi untuk mencegah input hanya berisi spasi
+        if input_text.strip() == "":
+            error_message = "Input tidak boleh hanya berisi spasi"
+        else:
+            matches = find_matches(input_text)
+            if matches:
+                result = Markup("<br>".join(matches))
+            else:
+                result = "Tidak ada hasil ditemukan"
+
+    return render_template("index.html", result=result, error_message=error_message)
 
 
 if __name__ == "__main__":
